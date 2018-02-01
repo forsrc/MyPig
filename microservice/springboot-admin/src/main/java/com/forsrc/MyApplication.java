75f7a72604c7ac2ad57cfbf0f3d488199e42ad15
forsrc <forsrc@gmail.com>
2018-02-02 00:18:00 +0900

    admin

--- microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.forsrc")
 @EnableAutoConfiguration
+@EnableAdminServer
 public class MyApplication {
 
     public static void main(String[] args) {
