af6c142aed752693cb4eb17be5b753b949f2903b
yes <forsrc@gmail.com>
2018-02-02 01:30:13 +0900

    Branch/init (#9)
    * config server
    
    * eureka
    
    * admin

--- microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
@@ -3,11 +3,16 @@ package com.forsrc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.forsrc")
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
+@EnableAdminServer
+@EnableDiscoveryClient
 public class MyApplication {
 
     public static void main(String[] args) {
