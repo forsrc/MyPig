afb32472df16f275eabb8f21ce70642b246b1792
forsrc <forsrc@gmail.com>
2018-02-02 01:22:59 +0900

    admin

--- ui/src/main/java/com/forsrc/MyApplication.java
+++ ui/src/main/java/com/forsrc/MyApplication.java
@@ -3,6 +3,7 @@ package com.forsrc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "com.forsrc")
 @EnableAutoConfiguration
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class MyApplication {
 
     public static void main(String[] args) {
