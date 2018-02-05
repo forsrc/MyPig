d886f3fd85ce5a0bfd13e7c1c6a5a59064255347
yes <forsrc@gmail.com>
2018-02-05 18:39:53 +0900

    Update MyApplication.java

--- microservice/springboot-sso-server/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/MyApplication.java
@@ -3,22 +3,16 @@ package com.forsrc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.SessionAttributes;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.forsrc")
 @EnableAutoConfiguration
 @EnableEurekaClient
 @EnableDiscoveryClient
-@SessionAttributes("authorizationRequest")
-@EnableResourceServer
 public class MyApplication {
 
     public static void main(String[] args) {
