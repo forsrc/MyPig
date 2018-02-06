481d4db2149d2656ae1662ee45e521bc58aa373a
yes <forsrc@gmail.com>
2018-02-06 19:44:31 +0900

    Update SsoService.java

--- microservice/springboot-sso-server/src/main/java/com/forsrc/sso/service/SsoService.java
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/sso/service/SsoService.java
@@ -2,23 +2,25 @@ package com.forsrc.sso.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.forsrc.sso.domain.entity.Authority;
 import com.forsrc.sso.domain.entity.User;
 
 @Service
-@Transactional
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public interface SsoService {
 
+    @Transactional(readOnly = true)
     public User getUserByUsername(String username);
 
     public void save(User entity);
 
     public void update(User entity);
 
+    @Transactional(readOnly = true)
     public List<Authority> getAuthorityByUsername(String username);
 
     public void save(Authority entity);
