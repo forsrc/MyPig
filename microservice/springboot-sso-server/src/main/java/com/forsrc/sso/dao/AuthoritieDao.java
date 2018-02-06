766d542e683d09cf4342dd88cfc3272c545a83d1
yes <forsrc@gmail.com>
2018-02-06 18:52:41 +0900

    Update AuthoritieDao.java

--- microservice/springboot-sso-server/src/main/java/com/forsrc/sso/dao/AuthoritieDao.java
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/sso/dao/AuthoritieDao.java
@@ -3,9 +3,9 @@ package com.forsrc.sso.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.forsrc.sso.domain.entity.Authoritie;
+import com.forsrc.sso.domain.entity.Authority;
 
 @Repository
-public interface AuthoritieDao extends JpaRepository<Authoritie, String> {
+public interface AuthorityDao extends JpaRepository<Authority, String> {
 
 }
