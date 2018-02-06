cf9712db026681a5406d41a7320df1879169650b
yes <forsrc@gmail.com>
2018-02-06 18:51:56 +0900

    Update SsoService.java

--- microservice/springboot-sso-server/src/main/java/com/forsrc/sso/service/SsoService.java
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/sso/service/SsoService.java
@@ -1,28 +1,62 @@
-package com.forsrc.sso.service;
+package com.forsrc.sso.service.impl;
 
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import com.forsrc.sso.dao.AuthoritieDao;
+import com.forsrc.sso.dao.UserDao;
 import com.forsrc.sso.domain.entity.Authority;
 import com.forsrc.sso.domain.entity.User;
+import com.forsrc.sso.service.SsoService;
 
 @Service
 @Transactional
-public interface SsoService {
+public class SsoServiceImpl implements SsoService {
 
-    public User getUserByUsername(String username);
+    @Autowired
+    private UserDao userDao;
 
-    public void save(User entity);
+    @Autowired
+    private AuthoritieDao authoritiesDao;
 
-    public void update(User entity);
+    @Override
+    public void save(User entity) {
+        userDao.save(entity);
 
-    public List<Authority> getAuthoritieByUsername(String username);
+    }
 
-    public void save(Authority entity);
+    @Override
+    public void update(User entity) {
+        userDao.save(entity);
+    }
 
-    public void update(Authority entity);
+    @Override
+    public void save(Authority entity) {
+        authoritiesDao.save(entity);
+    }
+
+    @Override
+    public void update(Authority entity) {
+        authoritiesDao.save(entity);
+
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userDao.findOne(username);
+    }
+
+    @Override
+    public List<Authority> getAuthoritieByUsername(String username) {
+        Authority entity = new Authority();                         
+        entity.setUsername(username);                          
+        Example<Authority> example = Example.of(entity);
+        return authoritiesDao.findAll(example);
+    }
 
 }
