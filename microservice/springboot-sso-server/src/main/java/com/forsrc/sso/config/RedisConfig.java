23a2ac697b0b858cad3c5b6fd890bbe9c64c2f99
forsrc <forsrc@gmail.com>
2018-02-08 07:37:43 +0900

    @EnableCaching

--- microservice/springboot-sso-server/src/main/java/com/forsrc/sso/config/RedisConfig.java
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/sso/config/RedisConfig.java
@@ -2,6 +2,8 @@ package com.forsrc.sso.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -9,6 +11,7 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@EnableCaching(mode = AdviceMode.PROXY)
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
