8faac11ed09e7a28039407b9f6413cb088959d31
forsrc <forsrc@gmail.com>
2018-02-08 16:58:22 +0900

    Create ExceptionControllerAdvice.java

---
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/sso/aop/ExceptionControllerAdvice.java
@@ -0,0 +1,46 @@
+package com.forsrc.sso.aop;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+    public static class Err {
+
+        private String message;
+
+        public Err(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionControllerAdvice.class);
+
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Err> handlePostNotFound(HttpServletRequest request, HttpServletResponse response, Exception ex) {
+        LOGGER.error("--> {} : {}", request.getRequestURI(), ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new Err(ex.getMessage()));
+    }
+}
