36900a5e6173cf5c8cdf4e8a9c644b10e1db06e0
forsrc <forsrc@gmail.com>
2018-03-05 22:28:31 +0900

    ui

--- ui/src/main/resources/public/mypig/www/js/app.js
+++ ui/src/main/resources/public/mypig/www/js/app.js
@@ -8,7 +8,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // Hide the accessory bar by default (remove this to show the accessory bar
+    // above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
