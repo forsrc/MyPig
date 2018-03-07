b5b072e55af446b57cecaf94e699dc501600bd61
forsrc <forsrc@gmail.com>
2018-03-07 14:36:22 +0900

    Update bootstrap.js

--- ui/src/main/resources/public/mypig/www/app/bootstrap.js
+++ ui/src/main/resources/public/mypig/www/app/bootstrap.js
@@ -5,9 +5,13 @@
 define([
     'require',
     'angular',
+    'ionic',
+    'ionic-angular',
+    'ng-cordova',
+    'console',
     'app',
     'routes'
-], function (require, ng) {
+], function (require, angular, ionic, ionicAngular, ngCordova, console, app, routes) {
     'use strict';
 
     /*
@@ -16,7 +20,38 @@ define([
      */
 
     require(['domReady!'], function (document) {
-        console.log("--> bootstarap...")
-        ng.bootstrap(document, ['app']);
+        console.group("bootstarap");
+        console.time("bootstarap");
+        console.debug("--> bootstarap...")
+        var _console = console;
+        angular.element(window.document).ready(function() {
+            _console.debug("angular.element(document).ready()");
+            var angularModule = app;
+            _console.debug("angularModule:", angularModule);
+            angularModule.run(function($ionicPlatform) {
+                console.log("angularModule.run():", $ionicPlatform);
+                $ionicPlatform.ready(function() {
+                    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                    // for form inputs)
+                    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                    }
+                    if (window.StatusBar) {
+                        // org.apache.cordova.statusbar required
+                        StatusBar.styleDefault();
+                    }
+                });
+            });
+            angularModule.config(function($mdThemingProvider, $mdGestureProvider) {
+                console.log("app.config()", $mdThemingProvider, $mdGestureProvider);
+                $mdGestureProvider.skipClickHijack();
+                $mdThemingProvider.theme('default').primaryPalette('red').accentPalette('blue');
+            });
+        });
+        angular.bootstrap(document, ['app']);
+
+ 
+        console.timeEnd("bootstarap");
+        console.groupEnd();
     });
 });
