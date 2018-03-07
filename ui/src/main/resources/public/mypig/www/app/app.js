79dc9547a9fc4cd8450c16f94d5bf6d22c021f3f
forsrc <forsrc@gmail.com>
2018-03-07 14:35:59 +0900

    Update app.js

--- ui/src/main/resources/public/mypig/www/app/app.js
+++ ui/src/main/resources/public/mypig/www/app/app.js
@@ -4,19 +4,55 @@
  */
 define([
     'angular',
-    'angular-route',
     './controllers/index',
     './directives/index',
     './filters/index',
-    './services/index'
+    './services/index',
+    'ionic',
+    'ionic-angular',
+    'ng-cordova',
+    'console',
+    'angular-route',
+    'angular-resource',
+    'angular-animate',
+    'angular-aria',
+    'angular-sanitize',
+    'angular-material',
+    'angular-material-icons',
+    'angular-ui-router',
+    'angular-material-icons',
+    'angular-material',
+    'angular-material-icons',
+    'angular-messages',
+    'angular-message-format',
+    'angular-require',
+    'angular-cookies',
+    'ngStorage',
+    'svg-assets-cache'
 ], function (angular) {
     'use strict';
+    console.log('--> app...')
 
-    return angular.module('app', [
+    var app =angular.module('app', [
+        'ngResource',
         'app.controllers',
         'app.directives',
         'app.filters',
         'app.services',
-        'ngRoute'
+        'ngRoute',
+        'ionic',
+        'ng',
+        'ngRequire',
+        'ui.router',
+        'ngMaterial',
+        'ngCordova',
+        'ngStorage',
+        'ngAnimate',
+        'ngSanitize',
+        'ngMdIcons',
+        'ngAria',
+        'ngMessages'
     ]);
+
+    return app;
 });
