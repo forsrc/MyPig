f42c8a016dfe6128737c83a000a92fdd33b846e3
forsrc <forsrc@gmail.com>
2018-03-08 15:34:47 +0900

    Update routes.js

--- ui/src/main/resources/public/mypig/www/app/routes.js
+++ ui/src/main/resources/public/mypig/www/app/routes.js
@@ -3,37 +3,46 @@
  * The routes you see here will be anchors '#/' unless specifically configured otherwise.
  */
 
-define(['./app'], function (app) {
+define(['./app', 'services/DependencyResolverFor'], function (app, dependencyResolverFor) {
     'use strict';
     return app.config(['$routeProvider', '$mdThemingProvider', '$stateProvider', '$requireProvider', function ($routeProvider, $mdThemingProvider, $stateProvider, $requireProvider) {
-        $mdThemingProvider
-            .theme('default')
-            .primaryPalette('red')
-            .accentPalette('blue');
+            $mdThemingProvider
+                .theme('default')
+                .primaryPalette('red')
+                .accentPalette('blue');
 
-        $routeProvider.otherwise({
-            redirectTo: '/login'
-        });
+            $routeProvider.otherwise({
+                redirectTo: '/login'
+            });
 
-//        $routeProvider.when('/login', {
-//            templateUrl: 'app/views/login.html',
-//            controller: 'LoginCtrl'
-//        });
 
-        $stateProvider.state("/login", {
-            url : "/login",
-            templateUrl : "app/views/login.html",
-            title : "Login",
-            controller : "LoginCtrl",
-            //controllerAs: "LoginCtrl",
-            resolve : {
-//                 css: $requireProvider.requireCSS([
-//                     'css!css/login.css'
-//                 ]),
-                deps : $requireProvider.requireJS([
-                        'controllers/login-controller'
-                    ])
-            }
-        })
+           $routeProvider.when('/login', {
+                templateUrl : 'app/views/login.html',
+                resolve : dependencyResolverFor([
+                    'controllers/login-controller'
+                ])
+            });
+
+            // $routeProvider.when('/login', {
+            //     templateUrl : 'app/views/login.html',
+            //     controller : 'LoginCtrl'
+            // });
+
+//        $stateProvider.state("/login", {
+//            url : "/login",
+//            templateUrl : "app/views/login.html",
+//            title : "Login",
+//            controller : "LoginCtrl",
+//            //controllerAs: "LoginCtrl",
+//            resolve : {
+////                 css: $requireProvider.requireCSS([
+////                     'css!css/login.css'
+////                 ]),
+//                deps : $requireProvider.requireJS([
+//                        'controllers/login-controller'
+//                    ])
+//            }
+//        })
     }]);
+
 });
