ac0af983e27771fc0786cf229aa412cbeca1897b
forsrc <forsrc@gmail.com>
2018-03-09 13:20:27 +0900

    Update routes.js

--- ui/src/main/resources/public/mypig/www/app/routes.js
+++ ui/src/main/resources/public/mypig/www/app/routes.js
@@ -3,9 +3,34 @@
  * The routes you see here will be anchors '#/' unless specifically configured otherwise.
  */
 
-define(['./app', 'services/DependencyResolverFor'], function (app, dependencyResolverFor) {
+define(['angular', './app', 'services/DependencyResolverFor'], function (angular, app, dependencyResolverFor) {
     'use strict';
-    return app.config(['$routeProvider', '$mdThemingProvider', '$stateProvider', '$requireProvider', function ($routeProvider, $mdThemingProvider, $stateProvider, $requireProvider) {
+    return app.config([
+        '$routeProvider',
+        '$locationProvider',
+        '$controllerProvider',
+        '$compileProvider',
+        '$filterProvider',
+        '$provide',
+        '$mdThemingProvider',
+        '$stateProvider',
+        '$requireProvider', function (
+                $routeProvider,
+                $locationProvider,
+                $controllerProvider,
+                $compileProvider,
+                $filterProvider,
+                $provide,
+                $mdThemingProvider,
+                $stateProvider,
+                $requireProvider) {
+
+            app.controller = $controllerProvider.register;
+            app.directive  = $compileProvider.directive;
+            app.filter     = $filterProvider.register;
+            app.factory    = $provide.factory;
+            app.service    = $provide.service;
+
             $mdThemingProvider
                 .theme('default')
                 .primaryPalette('red')
@@ -16,33 +41,33 @@ define(['./app', 'services/DependencyResolverFor'], function (app, dependencyRes
             });
 
 
-//           $routeProvider.when('/login', {
-//                templateUrl : 'app/views/login.html',
-//                resolve : dependencyResolverFor([
-//                    'controllers/LoginController'
-//                ])
-//            });
+           $routeProvider.when('/login', {
+                templateUrl : 'app/views/login.html',
+                resolve : dependencyResolverFor([
+                    'controllers/LoginController'
+                ])
+            });
 
             // $routeProvider.when('/login', {
             //     templateUrl : 'app/views/login.html',
             //     controller : 'LoginCtrl'
             // });
 
-        $stateProvider.state("/login", {
-            url : "/login",
-            templateUrl : "app/views/login.html",
-            title : "Login",
-            controller : "loginController",
-            //controllerAs: "LoginCtrl",
-            resolve : {
-//                 css: $requireProvider.requireCSS([
-//                     'css!css/login.css'
-//                 ]),
-                deps : $requireProvider.requireJS([
-                        'controllers/LoginController'
-                    ])
-            }
-        })
+//        $stateProvider.state("/login", {
+//            url : "/login",
+//            templateUrl : "app/views/login.html",
+//            title : "Login",
+//            controller : "loginController",
+//            //controllerAs: "LoginCtrl",
+//            resolve : {
+////                 css: $requireProvider.requireCSS([
+////                     'css!css/login.css'
+////                 ]),
+//                deps : $requireProvider.requireJS([
+//                        'controllers/LoginController'
+//                    ])
+//            }
+//        })
     }]);
 
 });
