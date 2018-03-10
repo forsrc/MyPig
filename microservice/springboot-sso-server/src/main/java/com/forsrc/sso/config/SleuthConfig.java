0ceaaa16946e437f99cc0c6a0ef1e45597af322c
forsrc <forsrc@gmail.com>
2018-03-10 19:09:34 +0900

    zipkin

---
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/sso/config/SleuthConfig.java
@@ -0,0 +1,15 @@
+package com.forsrc.sso.config;
+
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SleuthConfig {
+
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
+    }
+
+}
