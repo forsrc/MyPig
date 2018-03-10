fbe9fffca6653ca36c7aecf85d5f846903491505
forsrc <forsrc@gmail.com>
2018-03-11 01:34:17 +0900

    eureka

--- microservice/springboot-eureka-server/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-eureka-server/src/main/java/com/forsrc/MyApplication.java
@@ -3,13 +3,18 @@ package com.forsrc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.forsrc")
 //@EnableAutoConfiguration
 @EnableEurekaServer
+@EnableCloudDashboard
+@EnableDiscoveryClient
 public class MyApplication {
 
     public static void main(String[] args) {
