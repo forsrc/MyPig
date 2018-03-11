95487d5664a7bac905f8f42770aa490310e14ab1
forsrc <forsrc@gmail.com>
2018-03-12 00:49:53 +0900

    turbine

--- microservice/springboot-eureka-server/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-eureka-server/src/main/java/com/forsrc/MyApplication.java
@@ -1,14 +1,10 @@
 package com.forsrc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import org.springframework.context.annotation.ComponentScan;
 
 import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 
@@ -18,8 +14,6 @@ import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 @EnableEurekaServer
 @EnableCloudDashboard
 @EnableDiscoveryClient
-@EnableTurbine
-@EnableHystrix
 @EnableEurekaClient
 public class MyApplication {
 
