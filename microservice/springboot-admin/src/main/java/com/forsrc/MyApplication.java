b3e3bfee024166355b8b4b2b5bcc288d2fab64b9
forsrc <forsrc@gmail.com>
2018-03-12 15:59:05 +0900

    admin

--- microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
@@ -1,5 +1,12 @@
 package com.forsrc;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +15,12 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
@@ -20,7 +33,23 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 @EnableHystrixDashboard
 @EnableTurbine
 @EnableHystrix
+
 public class MyApplication {
+
+    @RestControllerAdvice
+    public static class IOExceptionControllerAdvice {
+        private static final Logger LOGGER = LoggerFactory.getLogger(IOExceptionControllerAdvice.class);
+        @ExceptionHandler(IOException.class)
+        @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+        public Object exceptionHandler(IOException e, HttpServletRequest request) {
+            LOGGER.error(e.getMessage());
+            if ("Broken pipe".equalsIgnoreCase(ExceptionUtils.getRootCauseMessage(e))) {
+                return null;
+            } else {
+                return new HttpEntity<>(e.getMessage());
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
