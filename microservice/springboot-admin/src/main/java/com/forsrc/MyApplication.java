35e45c51a787d57c63ec6864ac809460ae486960
forsrc <forsrc@gmail.com>
2018-03-13 23:37:34 +0900

    Update MyApplication.java

--- microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-admin/src/main/java/com/forsrc/MyApplication.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
