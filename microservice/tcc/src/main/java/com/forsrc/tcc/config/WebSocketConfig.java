bf68ee3b884bbc45a6c78ca31240650ac1fc5dcc
forsrc <forsrc@gmail.com>
2018-03-21 01:13:53 +0900

    ws user

--- microservice/tcc/src/main/java/com/forsrc/tcc/config/WebSocketConfig.java
+++ microservice/tcc/src/main/java/com/forsrc/tcc/config/WebSocketConfig.java
@@ -1,26 +1,18 @@
 package com.forsrc.tcc.config;
 
-import java.security.Principal;
-import java.util.Map;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptorAdapter;
 import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.server.RequestUpgradeStrategy;
-import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -28,8 +20,9 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic", "/user");
         config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
@@ -38,4 +31,23 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
                 .setAllowedOrigins("*")
                 .withSockJS();
     }
+
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(new ChannelInterceptorAdapter() {
+//
+//            @Override
+//            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+//
+//                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+//
+//                Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//                if (auth != null) {
+//                    accessor.setUser(auth);
+//                }
+//
+//                return message;
+//            }
+//        });
+//    }
 }
--- microservice/tcc/src/main/java/com/forsrc/tcc/ws/TccWebSocket.java
+++ microservice/tcc/src/main/java/com/forsrc/tcc/ws/TccWebSocket.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -21,6 +22,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
@@ -47,15 +49,31 @@ public class TccWebSocket {
 
     @MessageMapping("/tcc/{tccId}")
     @SendTo("/topic/tcc")
-    public String send( @DestinationVariable("tccId") String tccId, @Payload Message<String> message, SimpMessageHeaderAccessor headerAccessor, Principal user) throws Exception {
+    public String send(@DestinationVariable("tccId") String tccId, @Payload Message<String> message,
+            SimpMessageHeaderAccessor headerAccessor, Principal user) throws Exception {
         LOGGER.info("SimpMessageHeaderAccessor: {}", headerAccessor);
         LOGGER.info("Message: {}", message);
-        LOGGER.info("Principal: {}", user);
-        headerAccessor.getSessionId();
-        //messagingTemplate.
+        LOGGER.info("Principal: {} -> {}", user.getName(), user);
+        String sessionId = headerAccessor.getSessionId();
+        // messagingTemplate.
         String text = message.getPayload();
         Tcc tcc = tccService.get(StringUtils.toUuid(tccId));
+
+        messagingTemplate.convertAndSend("/topic/tcc", "test 1");
+        messagingTemplate.convertAndSend("/topic/tcc", "test 2", createHeaders(sessionId));
+        messagingTemplate.convertAndSendToUser(user.getName(), "/usermessage", "test 3");
+        
+        messagingTemplate.convertAndSendToUser(user.getName(), "/usermessage", "test messagingTemplate",
+                createHeaders(sessionId));
+
         return tcc != null ? tcc.toString() : tccId;
+    }
+
+    private MessageHeaders createHeaders(String sessionId) {
+        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
+        headerAccessor.setSessionId(sessionId);
+        headerAccessor.setLeaveMutable(true);
+        return headerAccessor.getMessageHeaders();
     }
 
     public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
@@ -86,13 +104,17 @@ public class TccWebSocket {
                 }).get(5, TimeUnit.SECONDS);
 
         System.out.println(session.isConnected());
-        CompletableFuture<String> completableFuture = new CompletableFuture<>();
-        session.subscribe("/topic/tcc", new TccStompSessionHandler(completableFuture));
+        CompletableFuture<String> completableFuture1 = new CompletableFuture<>();
+        CompletableFuture<String> completableFuture2 = new CompletableFuture<>();
+        session.subscribe("/topic/tcc", new TccStompSessionHandler(completableFuture1));
+        session.subscribe("/user/tcc/usermessage", new TccStompSessionHandler(completableFuture2));
 
         session.send("/app/tcc/d4e55207-db0a-4b8e-9691-90305cb51a44", "test");
 
-        LOGGER.info("ws: {}", completableFuture.get());
-
+        LOGGER.info("--> ws1: {}", completableFuture1.get());
+        LOGGER.info("--> ws2: {}", completableFuture2.get());
+        TimeUnit.SECONDS.sleep(5);
         session.disconnect();
     }
+
 }
