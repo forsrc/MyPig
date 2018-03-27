9ced3a785dc7517ff88e08851daac4fb33f6e29d
forsrc <forsrc@gmail.com>
2018-03-27 18:40:56 +0900

    Update RestConfig.java

--- microservice/tcc/src/main/java/com/forsrc/tcc/config/RestConfig.java
+++ microservice/tcc/src/main/java/com/forsrc/tcc/config/RestConfig.java
@@ -1,5 +1,6 @@
 package com.forsrc.tcc.config;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -8,11 +9,14 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.RequestContextListener;
 
@@ -62,11 +66,33 @@ public class RestConfig {
 
         OAuth2RestTemplate tccOAuth2RestTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext());
         //tccOAuth2RestTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter()));
+        tccOAuth2RestTemplate.setErrorHandler(new OAuth2ResponseErrorHandler(tccOAuth2RestTemplate));
         return tccOAuth2RestTemplate;
     }
 
     @Bean
     public RequestContextListener requestContextListener() {
         return new RequestContextListener();
+    }
+
+    static class OAuth2ResponseErrorHandler implements ResponseErrorHandler {
+
+        private OAuth2RestTemplate oAuth2RestTemplate;
+
+        public OAuth2ResponseErrorHandler(OAuth2RestTemplate oAuth2RestTemplate) {
+            this.oAuth2RestTemplate = oAuth2RestTemplate;
+        }
+
+        @Override
+        public boolean hasError(ClientHttpResponse response) throws IOException {
+            int statusCode = response.getStatusCode().value();
+            return statusCode == HttpStatus.UNAUTHORIZED.value();
+        }
+
+        @Override
+        public void handleError(ClientHttpResponse response) throws IOException {
+            oAuth2RestTemplate.getOAuth2ClientContext().setAccessToken(null);
+        }
+
     }
 }
