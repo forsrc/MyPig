4dfd7b05b08abce2fda68599991044f9578b6020
forsrc <forsrc@gmail.com>
2018-03-27 11:41:11 +0900

    Update RestConfig.java

--- microservice/tcc/src/main/java/com/forsrc/tcc/config/RestConfig.java
+++ microservice/tcc/src/main/java/com/forsrc/tcc/config/RestConfig.java
@@ -61,7 +61,7 @@ public class RestConfig {
         resourceDetails.setScope(Arrays.asList("read", "write"));
 
         OAuth2RestTemplate tccOAuth2RestTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext());
-        tccOAuth2RestTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter()));
+        //tccOAuth2RestTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter()));
         return tccOAuth2RestTemplate;
     }
 
