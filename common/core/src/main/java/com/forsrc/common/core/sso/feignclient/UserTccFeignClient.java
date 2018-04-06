bd57295704c8d37f27428b5a2a31b2f921a55662
forsrc <forsrc@gmail.com>
2018-04-06 18:49:39 +0900

    Branch/sso tcc (#44)
    * Tcc
    * FeignClient

--- common/core/src/main/java/com/forsrc/common/core/sso/feignclient/UserTccFeignClient.java
+++ common/core/src/main/java/com/forsrc/common/core/sso/feignclient/UserTccFeignClient.java
@@ -1,20 +1,14 @@
 package com.forsrc.common.core.sso.feignclient;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.forsrc.common.core.tcc.exception.TccException;
+import com.forsrc.common.core.tcc.feignclient.TccBaseFeignClient;
 import com.forsrc.sso.domain.entity.UserTcc;
 
 @FeignClient(name = "SPRINGBOOT-SSO-SERVER", path = "/sso/api/v1/tcc/user")
-public interface UserTccFeignClient {
+public interface UserTccFeignClient extends TccBaseFeignClient<UserTcc> {
 
-
+/*
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ResponseEntity<UserTcc> tccTry(@RequestBody UserTcc tcc, @RequestHeader("Authorization") String accessToken)
             throws TccException;
@@ -26,5 +20,5 @@ public interface UserTccFeignClient {
     @RequestMapping(value = "/cancel/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> cancel(@PathVariable("id") String id,
             @RequestHeader("Authorization") String accessToken) throws TccException;
-
+*/
 }
