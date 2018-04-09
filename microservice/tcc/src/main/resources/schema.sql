ef137518dbfb65bc42aeb2dd4e3b3c1dc1691cdb
forsrc <forsrc@gmail.com>
2018-04-09 13:10:28 +0900

    Update schema.sql

--- microservice/tcc/src/main/resources/schema.sql
+++ microservice/tcc/src/main/resources/schema.sql
@@ -2,26 +2,28 @@
 
 DROP TABLE IF EXISTS t_tcc;
 CREATE TABLE t_tcc (
-  id          UUID NOT NULL,
-  microservice varchar(200),
-  times       tinyint(2) NOT NULL,
-  status      tinyint(2) NOT NULL,
-  expire      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  create      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  id          UUID          NOT NULL,
+  microservice varchar(200) NOT NULL DEFAULT '',
+  times       tinyint(2)    NOT NULL DEFAULT 0,
+  status      tinyint(2)    NOT NULL DEFAULT 0,
+  version     int           NOT NULL DEFAULT 0,
+  expire      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  create      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 ) ; -- ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS t_tcc_link;
 CREATE TABLE t_tcc_link (
-  id          UUID NOT NULL,
-  tcc_id      UUID NOT NULL,
-  uri         varchar(200) NOT NULL,
+  id          UUID          NOT NULL,
+  tcc_id      UUID          NOT NULL,
+  uri         varchar(200)  NOT NULL,
   path        varchar(500)  NOT NULL,
-  status      tinyint(2) NOT NULL,
-  expire      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  create      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status      tinyint(2)    NOT NULL DEFAULT 0,
+  version     int           NOT NULL DEFAULT 0,
+  expire      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  create      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 ) ; -- ENGINE=InnoDB DEFAULT CHARSET=latin1;
