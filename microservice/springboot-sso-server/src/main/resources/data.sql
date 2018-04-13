fa05f9fae42080d5f19801eac77342f63b0caef6
forsrc <forsrc@gmail.com>
2018-04-13 11:15:12 +0900

    Update data.sql

--- microservice/springboot-sso-server/src/main/resources/data.sql
+++ microservice/springboot-sso-server/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-
+delete from oauth_client_details;
 -- oauth_client_details
 -- INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove) VALUES  ('forsrc', 'forsrc', 'forsrc', 'forsrc,read,write', 'authorization_code,client_credentials,refresh_token,password,implicit', null, null, 36000, 36000, null, true);
 INSERT INTO oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove) VALUES  ('sso',   null,   '$2a$10$Smc1lKpNSr/MeX1ZTt0GVu0b6LzlbOBp8Lzy9JfriAb1Xp8rVBMsm', 'sso,read,write',    'authorization_code,client_credentials,refresh_token,password,implicit', null, null, 36000, 36000, null, true);
@@ -12,10 +12,10 @@ INSERT INTO oauth_client_details (client_id, resource_ids, client_secret, scope,
 
 
 -- users
-INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'forsrc@gmail.com' username, '$2a$10$Wzme7qZtAsJZspQpNx3ee.qTu/IqRHiTb0jORWUOXCxptAkG3kf8e' password, 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'forsrc@gmail.com');
-INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'user'             username, '$2a$10$SNKOBpTBuCbWukZ3Rc5DpuIHRP585Ss02fULAIX/m1NmFpWeJ8ic2' password, 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'user');
-INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'tcc'              username, '$2a$10$lFUTwK/W3S3U8NI3cnqJPeVD3cZj6udLbW2W5GMvybtJw70N4WqFC' password, 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'tcc');
-INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'test'             username, '$2a$10$uCchlP6N1q7ZOEMMifeZyOEOgqpddiVEIiIrM4k/76ftgLxtBaSXq' password, 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'test');
+INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'forsrc@gmail.com' username, '$2a$10$Wzme7qZtAsJZspQpNx3ee.qTu/IqRHiTb0jORWUOXCxptAkG3kf8e' "password", 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'forsrc@gmail.com');
+INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'user'             username, '$2a$10$SNKOBpTBuCbWukZ3Rc5DpuIHRP585Ss02fULAIX/m1NmFpWeJ8ic2' "password", 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'user');
+INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'tcc'              username, '$2a$10$lFUTwK/W3S3U8NI3cnqJPeVD3cZj6udLbW2W5GMvybtJw70N4WqFC' "password", 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'tcc');
+INSERT INTO users (username, password, enabled) SELECT * FROM (SELECT 'test'             username, '$2a$10$uCchlP6N1q7ZOEMMifeZyOEOgqpddiVEIiIrM4k/76ftgLxtBaSXq' "password", 1 enabled) AS T WHERE NOT EXISTS (SELECT username FROM users WHERE username = 'test');
 
 -- authorities
 INSERT INTO authorities (username, authority) SELECT * FROM (SELECT 'forsrc@gmail.com' username, 'ROLE_ADMIN' authorities) AS T WHERE NOT EXISTS (SELECT username FROM authorities WHERE username = 'forsrc@gmail.com');
@@ -25,7 +25,11 @@ INSERT INTO authorities (username, authority) SELECT * FROM (SELECT 'tcc'       
 INSERT INTO authorities (username, authority) SELECT * FROM (SELECT 'test'             username, 'ROLE_TEST'  authorities) AS T WHERE NOT EXISTS (SELECT username FROM authorities WHERE username = 'test');
 
 -- t_sso_user_tcc
-INSERT INTO t_sso_user_tcc (id, username, password, enabled, authorities, expire, status) SELECT * FROM (SELECT '5112cdecfec04f2e8d364ed1d14b7882' id, 'A' username, '$2a$10$Wzme7qZtAsJZspQpNx3ee.qTu/IqRHiTb0jORWUOXCxptAkG3kf8e' password, 1 enabled, 'ROLE_USER' authorities, TIMESTAMPADD(MINUTE, 5, CURRENT_TIMESTAMP) expire, 0 status) AS T WHERE NOT EXISTS (SELECT username FROM t_sso_user_tcc WHERE id = '5112cdecfec04f2e8d364ed1d14b7882');
-UPDATE t_sso_user_tcc SET expire = TIMESTAMPADD(MINUTE, 5, CURRENT_TIMESTAMP), status = 0 WHERE id = '5112cdecfec04f2e8d364ed1d14b7882';
+--INSERT INTO t_sso_user_tcc (id, username, "password", enabled, authorities, expire, status) SELECT * FROM (SELECT '5112cdecfec04f2e8d364ed1d14b7882' id, 'A' username, '$2a$10$Wzme7qZtAsJZspQpNx3ee.qTu/IqRHiTb0jORWUOXCxptAkG3kf8e' "password", 1 enabled, 'ROLE_USER' authorities, TIMESTAMPADD(MINUTE, 5, CURRENT_TIMESTAMP) expire, 0 status) AS T WHERE NOT EXISTS (SELECT username FROM t_sso_user_tcc WHERE id = '5112cdecfec04f2e8d364ed1d14b7882');
+INSERT INTO t_sso_user_tcc (id, username, "password", enabled, authorities, expire, status) SELECT * FROM (SELECT uuid_in('5112cdecfec04f2e8d364ed1d14b7882') id, 'A' username, '$2a$10$Wzme7qZtAsJZspQpNx3ee.qTu/IqRHiTb0jORWUOXCxptAkG3kf8e' "password", 1 enabled, 'ROLE_USER' authorities, (CURRENT_TIMESTAMP + '5 minute') expire, 0 status) AS T WHERE NOT EXISTS (SELECT username FROM t_sso_user_tcc WHERE id = '5112cdecfec04f2e8d364ed1d14b7882');
+
+--UPDATE t_sso_user_tcc SET expire = TIMESTAMPADD(MINUTE, 5, CURRENT_TIMESTAMP), status = 0 WHERE id = '5112cdecfec04f2e8d364ed1d14b7882';
+UPDATE t_sso_user_tcc SET expire = (CURRENT_TIMESTAMP + '5 minute'), status = 0 WHERE id = '5112cdecfec04f2e8d364ed1d14b7882';
+
 
 
