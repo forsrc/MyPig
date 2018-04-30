59396095448b7446c8d763598be15d0c2709302d
forsrc <forsrc@gmail.com>
2018-04-30 23:48:22 +0900

    spring data rest

--- microservice/tcc/src/main/java/com/forsrc/tcc/dao/TccLinkDao.java
+++ microservice/tcc/src/main/java/com/forsrc/tcc/dao/TccLinkDao.java
@@ -3,11 +3,14 @@ package com.forsrc.tcc.dao;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.forsrc.tcc.domain.entity.TccLink;
 
 @Repository
-public interface TccLinkDao extends JpaRepository<TccLink, UUID> {
+@RepositoryRestResource(collectionResourceRel = "tccLink", path = "tccLink")
+public interface TccLinkDao extends JpaRepository<TccLink, UUID>, PagingAndSortingRepository<TccLink, UUID> {
 
 }
