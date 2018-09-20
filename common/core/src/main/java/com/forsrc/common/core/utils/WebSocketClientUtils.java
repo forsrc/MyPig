3fe7205e6632ff8217c3f6c353bb1e00536c0cb0
forsrc <forsrc@gmail.com>
2018-09-20 23:58:28 +0800

    WebSocketClientUtils

--- common/core/src/main/java/com/forsrc/common/core/utils/WebSocketClientUtils.java
+++ common/core/src/main/java/com/forsrc/common/core/utils/WebSocketClientUtils.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.task.SyncTaskExecutor;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.stomp.StompSession;
@@ -20,14 +22,16 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 public class WebSocketClientUtils {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketClientUtils.class);
+
     private StompSession session;
     private WebSocketStompClient stompClient;
 
     private WebSocketClientUtils() {
     }
 
-    public static WebSocketClientUtils get(String ws, MessageConverter messageConverter, OAuth2RestTemplate restTemplate)
-            throws InterruptedException, ExecutionException, TimeoutException {
+    public static WebSocketClientUtils get(String ws, MessageConverter messageConverter,
+            OAuth2RestTemplate restTemplate) throws InterruptedException, ExecutionException, TimeoutException {
         return new WebSocketClientUtils().set(ws, messageConverter, restTemplate);
     }
 
@@ -46,6 +50,7 @@ public class WebSocketClientUtils {
         session = stompClient
                 .connect(String.format("%s?access_token=%s", ws, accessToken), new StompSessionHandlerAdapter() {
                 }).get(1, TimeUnit.SECONDS);
+        LOGGER.info("ws session id: {}", session.getSessionId());
         return this;
     }
 
@@ -54,23 +59,57 @@ public class WebSocketClientUtils {
     }
 
     public WebSocketClientUtils set(String topic, StompSessionHandlerAdapter sessionHandlerAdapter) {
-        session.subscribe(topic, sessionHandlerAdapter);
+        if (session == null || !session.isConnected()) {
+            LOGGER.error("ws session is null");
+            throw new RuntimeException("ws session is null");
+        }
+        try {
+            session.subscribe(topic, sessionHandlerAdapter);
+        } catch (RuntimeException e) {
+            LOGGER.error("ws runtimeException: {}", e.getMessage());
+            disconnect();
+            throw e;
+        } finally {
+        }
         return this;
     }
 
     public WebSocketClientUtils send(String topic, Object message) {
-        session.send(topic, message);
+        if (session == null || !session.isConnected()) {
+            LOGGER.error("ws session is null");
+            throw new RuntimeException("ws session is null");
+        }
+        try {
+            session.send(topic, message);
+        } catch (RuntimeException e) {
+            LOGGER.error("ws runtimeException: {}", e.getMessage());
+            disconnect();
+            throw e;
+        } finally {
+        }
         return this;
     }
 
-    public WebSocketClientUtils handle(Handler handler) {
-        handler.handle(session);
+    public WebSocketClientUtils handle(Handler handler) throws RuntimeException {
+        if (session == null || !session.isConnected()) {
+            LOGGER.error("ws session is null");
+            throw new RuntimeException("ws session is null");
+        }
+        try {
+            handler.handle(session);
+        } catch (RuntimeException e) {
+            LOGGER.error("ws runtimeException: {}", e.getMessage());
+            disconnect();
+            throw e;
+        } finally {
+        }
         return this;
     }
 
     public void disconnect() {
         if (session != null && session.isConnected()) {
             session.disconnect();
+            session = null;
         }
     }
 
