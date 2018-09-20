b2ccf66bc7329d8a6a8af6846e7ef50681812907
forsrc <forsrc@gmail.com>
2018-09-20 15:39:00 +0800

    spring cloud config

--- microservice/springboot-config-server/src/main/java/com/forsrc/MyApplication.java
+++ microservice/springboot-config-server/src/main/java/com/forsrc/MyApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //@ComponentScan(basePackages = "com.forsrc")
 @EnableAutoConfiguration
 @EnableConfigServer
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableDiscoveryClient
 public class MyApplication {
 
