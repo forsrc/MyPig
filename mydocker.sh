3714c284be5e83333afb0c07b2f66da437b71c7c
forsrc <forsrc@gmail.com>
2019-05-05 20:26:27 +0800

    docker

--- mydocker.sh
+++ mydocker.sh
@@ -3,41 +3,41 @@ mvn install -DskipTests
 cd microservice/springboot-eureka-server
 mvn dockerfile:build -DskipTests
 #mvn dockerfile:push
-docker push forsrc/forsrc-mypig-microservice-springboot-eureka-server
+#docker push forsrc/forsrc-mypig-microservice-springboot-eureka-server
 cd ../../
 
 cd microservice/springboot-admin
 mvn dockerfile:build -DskipTests
 #mvn dockerfile:push
-docker push forsrc/forsrc-mypig-microservice-springboot-admin
+#docker push forsrc/forsrc-mypig-microservice-springboot-admin
 cd ../../
 
 cd microservice/springboot-zipkin-server
 mvn dockerfile:build -DskipTests
 #mvn dockerfile:push
-docker push forsrc/forsrc-mypig-microservice-springboot-zipkin-server
+#docker push forsrc/forsrc-mypig-microservice-springboot-zipkin-server
 cd ../../
 
 cd microservice/springboot-config-server
 mvn dockerfile:build -DskipTests
 #mvn dockerfile:push
-docker push forsrc/forsrc-mypig-microservice-springboot-config-server
+#docker push forsrc/forsrc-mypig-microservice-springboot-config-server
 cd ../../
 
 cd microservice/springboot-sso-server
 mvn dockerfile:build -DskipTests
 #mvn dockerfile:push
-docker push forsrc/forsrc-mypig-microservice-springboot-sso-server
+#docker push forsrc/forsrc-mypig-microservice-springboot-sso-server
 cd ../../
 
 cd microservice/springboot-api-gateway
 mvn dockerfile:build -DskipTests
 #mvn dockerfile:push
-docker push forsrc/forsrc-mypig-microservice-springboot-api-gateway
+#docker push forsrc/forsrc-mypig-microservice-springboot-api-gateway
 cd ../../
 
 cd microservice/tcc
 mvn dockerfile:build -DskipTests
 #mvn dockerfile:push
-docker push forsrc/forsrc-mypig-microservice-tcc
+#docker push forsrc/forsrc-mypig-microservice-tcc
 cd ../../
