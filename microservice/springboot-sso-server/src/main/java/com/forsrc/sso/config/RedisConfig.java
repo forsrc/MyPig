1451946840af2c88889c382a755a474d22240c86
forsrc <forsrc@gmail.com>
2019-05-05 22:42:34 +0800

    docker

--- microservice/springboot-sso-server/src/main/java/com/forsrc/sso/config/RedisConfig.java
+++ microservice/springboot-sso-server/src/main/java/com/forsrc/sso/config/RedisConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -25,7 +27,16 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        // redisStandaloneConfiguration.setDatabase(database);
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        // redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder lettuceClientConfigurationBuilder = LettuceClientConfiguration.builder();
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisStandaloneConfiguration,
+                lettuceClientConfigurationBuilder.build());
+        return factory;
     }
 
     @Bean
