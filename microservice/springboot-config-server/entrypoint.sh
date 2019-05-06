69f301d2f99b691ff51076ccfa31d40d6438d4dc
forsrc <forsrc@gmail.com>
2019-05-06 13:42:53 +0800

    Branch/dev (#66)
    * docker

--- microservice/springboot-config-server/entrypoint.sh
+++ microservice/springboot-config-server/entrypoint.sh
@@ -4,7 +4,7 @@ cd app
 echo ====================== `hostname` start...
 echo \
      java \
-	 $JAVA_OPTS \
+     $JAVA_OPTS \
      -Djava.security.egd=file:/dev/./urandom \
      -jar app.jar \
      $PARAM_OPTS
