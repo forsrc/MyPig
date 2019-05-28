22bd925c6bbde8c7239731adf248d5c7ddd33624
forsrc <forsrc@gmail.com>
2019-05-28 14:50:08 +0800

    resize

--- ui/src/main/ui/src/app/user/form/user.component.ts
+++ ui/src/main/ui/src/app/user/form/user.component.ts
@@ -1,10 +1,13 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from "../../service/user.service";
 import {Location} from '@angular/common';
 import {MatDrawer, MatTreeNestedDataSource} from "@angular/material";
 import {NestedTreeControl} from '@angular/cdk/tree';
 import {User} from "../user.model";
+
+
+(window as any).global = window;
 
 @Component({
   selector: 'app-home',
@@ -40,9 +43,20 @@ export class UserComponent implements OnInit {
     this.isUpdate = this.user.username != ":usernmae";
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    let width = event.target.innerWidth;
+    console.log("window:resize -> " + width);
+    if (width < 1000 ) {
+      this.drawer.toggle(false);
+    } else {
+      this.drawer.toggle(true);
+    }
+  }
+
   ngOnInit() {
     //alert(id);
-    this.drawer.toggle(true);
+    this.drawer.toggle(window.innerWidth > 1000);
     this.treeDate = <TreeNode[]>[
       {
         name: 'USER',
